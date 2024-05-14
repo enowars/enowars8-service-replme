aa1ef2bed36d91298c0aff07bfc81a2a72691a66
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-05-14 12:32:35 +0200

    feat(checker): implement putflag(0) and getflag(0)

--- checker/checker.py
+++ checker/checker.py
@@ -1,7 +1,4 @@
 from asyncio import sleep
-import secrets
-import base64
-import websockets
 from logging import LoggerAdapter
 from typing import Optional
 
@@ -14,6 +11,8 @@ from enochecker3.types import (
 )
 from enochecker3.utils import FlagSearcher, assert_equals, assert_in
 from httpx import AsyncClient
+
+from util import create_terminal, create_user, terminal_websocket, user_login
 
 checker = Enochecker("cafedodo", 6969)
 
@@ -29,76 +28,46 @@ async def putflag_test(
     db: ChainDB,
     logger: LoggerAdapter,
 ) -> str:
-    username = secrets.token_hex(30)
-    password = secrets.token_hex(30)
-
-    await db.set("credentials", (username, password))
-
-    logger.info(f"Creating user: {username}:{password}")
-    response = await client.post(
-        "/api/login/private",
-        data={"username": username, "password": password},
-        follow_redirects=True,
+    (username, password, port) = await create_user(client, db, logger)
+    pid = await create_terminal(client, logger, username, password)
+    await terminal_websocket(
+        task.address,
+        logger,
+        username,
+        password,
+        port,
+        pid,
+        [
+            (f'echo "{task.flag}" > flagstore.txt && echo OK\n', ".*\nOK.*"),
+        ],
     )
-    logger.info(f"Server answered: {response.status_code} - {response.text}")
-
-    assert_equals(response.status_code < 300, True, "Creating user failed")
-
-    json = response.json()
-    port = json["port"]
-    assert_equals(port is not None, True, "Did not receive a port")
-    logger.info(f"Port: {port}")
-
-    logger.info("Creating terminal")
-    for _ in range(5):
-        response = await client.post(
-            "/api/terminal",
-            headers={
-                "Authorization": "Basic "
-                + base64.b64encode(f"{username}:{password}".encode("utf-8")).decode("utf-8")
-            },
-            json={"cols": 80, "rows": 25},
-            follow_redirects=True,
-        )
-        if response.status_code < 300:
-            break
-    logger.info(f"Server answered: {response.status_code} - {response.text}")
-
-    assert_equals(response.status_code < 300, True, "Creating terminal failed")
-
-    pid = response.text
-    assert_equals(pid is not None, True, "Did not receive a PID")
-    logger.info(f"PID: {pid}")
-
-    url = f"ws://{task.address}:6969/ws/terminal/{port}/{pid}"
-    async with websockets.connect(url) as websocket:
-        await sleep(0.1)
-        await websocket.send(username + '\n')
-        await sleep(0.1)
-        await websocket.send(password + '\n')
-        await sleep(0.1)
-        message = await websocket.recv()
-        while message is not None:
-            logger.info(f"Message: {message}")
-            message = await websocket.recv()
 
     return username
 
 
 @checker.getflag(0)
 async def getflag_test(
-    task: GetflagCheckerTaskMessage, client: AsyncClient, db: ChainDB
+    task: GetflagCheckerTaskMessage,
+    client: AsyncClient,
+    db: ChainDB,
+    logger: LoggerAdapter,
 ) -> None:
-    try:
-        token = await db.get("token")
-    except KeyError:
-        raise MumbleException("Missing database entry from putflag")
+    (username, password, port) = await user_login(client, db, logger)
+    pid = await create_terminal(client, logger, username, password)
+    await terminal_websocket(
+        task.address,
+        logger,
+        username,
+        password,
+        port,
+        pid,
+        [
+            ("cat flagstore.txt\n", f".*\n{task.flag}.*"),
+        ],
+    )
 
-    r = await client.get(f"/note/{token}")
-    assert_equals(r.status_code, 200, "getting note with flag failed")
-    assert_in(task.flag, r.text, "flag missing from note")
 
-
+"""
 @checker.exploit(0)
 async def exploit_test(searcher: FlagSearcher, client: AsyncClient) -> Optional[str]:
     r = await client.get(
@@ -108,6 +77,7 @@ async def exploit_test(searcher: FlagSearcher, client: AsyncClient) -> Optional[
 
     if flag := searcher.search_flag(r.text):
         return flag
+"""
 
 
 if __name__ == "__main__":
---
+++ checker/util.py
@@ -0,0 +1,155 @@
+import asyncio
+from asyncio import sleep
+import base64
+import secrets
+import re
+from logging import LoggerAdapter
+from typing import List, Optional
+import websockets
+
+from enochecker3.chaindb import ChainDB
+from enochecker3.types import MumbleException, PutflagCheckerTaskMessage
+from enochecker3.utils import assert_equals
+from httpx import AsyncClient, Response
+
+
+async def create_user(
+    client: AsyncClient,
+    db: ChainDB,
+    logger: LoggerAdapter,
+) -> tuple[str, str, str]:
+    username = secrets.token_hex(30)
+    password = secrets.token_hex(30)
+
+    logger.info(f"Creating user: {username}:{password}")
+    response = await client.post(
+        "/api/login/private",
+        data={"username": username, "password": password},
+        follow_redirects=True,
+    )
+    logger.info(f"Server answered: {response.status_code} - {response.text}")
+
+    assert_equals(response.status_code < 300, True, "Creating user failed")
+
+    json = response.json()
+    port = str(json["port"])
+    assert_equals(port is not None, True, "Did not receive a port")
+    logger.info(f"Port: {port}")
+
+    await db.set("credentials", (username, password))
+
+    return (username, password, port)
+
+async def user_login(
+    client: AsyncClient,
+    db: ChainDB,
+    logger: LoggerAdapter,
+) -> tuple[str, str, str]:
+    try:
+        (username, password) = await db.get("credentials")
+    except KeyError:
+        raise MumbleException("Missing database entry from putflag")
+
+    logger.info(f"Login user: {username}:{password}")
+    response = await client.post(
+        "/api/login/private",
+        data={"username": username, "password": password},
+        follow_redirects=True,
+    )
+    logger.info(f"Server answered: {response.status_code} - {response.text}")
+
+    assert_equals(response.status_code < 300, True, "Creating user failed")
+
+    json = response.json()
+    port = str(json["port"])
+    assert_equals(port is not None, True, "Did not receive a port")
+    logger.info(f"Port: {port}")
+
+    return (username, password, port)
+
+
+async def create_terminal(
+    client: AsyncClient, logger: LoggerAdapter, username: str, password: str
+) -> str:
+    logger.info("Creating terminal")
+    response: Optional[Response] = None
+    for _ in range(5):
+        response = await client.post(
+            "/api/terminal",
+            headers={
+                "Authorization": "Basic "
+                + base64.b64encode(f"{username}:{password}".encode("utf-8")).decode(
+                    "utf-8"
+                )
+            },
+            json={"cols": 80, "rows": 25},
+            follow_redirects=True,
+        )
+        if response.status_code < 300:
+            break
+
+    if response is None:
+        raise MumbleException("Create terminal request failed")
+
+    logger.info(f"Server answered: {response.status_code} - {response.text}")
+
+    assert_equals(response.status_code < 300, True, "Creating terminal failed")
+
+    pid = response.text
+    assert_equals(pid is not None, True, "Did not receive a PID")
+    logger.info(f"PID: {pid}")
+
+    return pid
+
+
+async def websocket_recv_until(
+    websocket: websockets.WebSocketClientProtocol, pattern: str
+):
+    payload = ""
+    match = None
+
+    while match is None:
+        msg = await asyncio.wait_for(websocket.recv(), timeout=5)
+        if isinstance(msg, bytes):
+            payload += msg.decode("utf-8")
+        elif isinstance(msg, str):
+            payload += msg
+        else:
+            raise MumbleException("Websocket connection recv returned unexpected data")
+        match = re.match(pattern, payload, re.S)
+
+    return payload
+
+
+async def terminal_login(
+    logger: LoggerAdapter,
+    websocket: websockets.WebSocketClientProtocol,
+    username: str,
+    password: str,
+):
+    logger.info(await websocket_recv_until(websocket, ".*login:.*"))
+    await websocket.send(username + "\n")
+    logger.info(await websocket_recv_until(websocket, ".*Password:.*"))
+    await websocket.send(password + "\n")
+    logger.info(await websocket_recv_until(websocket, ".*Welcome to Alpine!.*"))
+
+
+async def terminal_websocket(
+    address: str,
+    logger: LoggerAdapter,
+    username: str,
+    password: str,
+    port: str,
+    pid: str,
+    actions: List[tuple[str, str]],
+):
+    url = f"ws://{address}:6969/ws/terminal/{port}/{pid}"
+    async with websockets.connect(url) as websocket:
+        await terminal_login(logger, websocket, username, password)
+        for action in actions:
+            print(action[0].encode("utf-8"))
+            await websocket.send(action[0])
+            print(action[1].encode("utf-8"))
+            logger.info(await websocket_recv_until(websocket, action[1]))
+
+    return ""
