018ad43ce83edae3bdb1c59ec9f561544b426736
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-06-06 14:31:00 +0200

    feat: WIP sessions

--- service/frontend/src/ts/index.ts
+++ service/frontend/src/ts/index.ts
@@ -1,5 +1,45 @@
 import { UserSessionResponseSchema } from "./lib"
 
+customElements.define(
+  'session-elem',
+  class extends HTMLElement {
+    constructor() {
+      super();
+      // @ts-ignore
+      const template = document.getElementById("session-template").content;
+      const shadowRoot = this.attachShadow({ mode: "open" });
+      [
+        '/static/css/style.css',
+        '/static/fontawesome/css/fontawesome.css',
+        '/static/fontawesome/css/brands.css',
+        '/static/fontawesome/css/solid.css'
+      ].forEach((href) => {
+        const linkElem = document.createElement('link');
+        linkElem.rel = 'stylesheet';
+        linkElem.href = href;
+        shadowRoot.appendChild(linkElem);
+      })
+      shadowRoot.appendChild(template.cloneNode(true));
+    }
+    connectedCallback() {
+      const hrefSlot = this.shadowRoot.querySelector('slot[name="href"]');
+      const dynamicLink = this.shadowRoot.getElementById('dynamic-link');
+
+      const updateHref = () => {
+        // @ts-ignore
+        const nodes = hrefSlot.assignedNodes();
+        if (nodes.length > 0) {
+          // @ts-ignore
+          dynamicLink.href = nodes[0].textContent;
+        }
+      };
+
+      hrefSlot.addEventListener('slotchange', updateHref);
+      updateHref();
+    }
+  },
+)
+
 fetch("/api/user/sessions")
   .then((response) => response.json())
   .then((data) => UserSessionResponseSchema.parse(data))
@@ -7,54 +47,25 @@ fetch("/api/user/sessions")
     const container = document.getElementById("session_container")
     names.forEach((name) => {
 
-      const row = document.createElement('div')
-      row.classList.add(
-        'flex',
-        'flex-row',
-        'w-full',
-        'justify-between'
-      )
+      const session = document.createElement('session-elem');
+      const labelSlot = document.createElement('slot');
+      labelSlot.slot = "label";
+      labelSlot.innerText = name.substring(0, 10) + "...";
+      const hrefSlot = document.createElement('slot');
+      hrefSlot.slot = "href";
+      hrefSlot.innerText = "/term/" + name;
+      session.appendChild(labelSlot)
+      session.appendChild(hrefSlot)
 
-      const label = document.createElement('div')
-      label.innerText = name.substring(0, 10);
-
-      const link = document.createElement('a')
-      link.href = "/term/" + name
-      link.classList.add(
-        "rounded",
-        "bg-indigo-600",
-        "px-2",
-        "py-1",
-        "font-semibold",
-        "text-white",
-        "shadow-sm",
-        "hover:bg-indigo-500",
-        "focus-visible:outline",
-        "focus-visible:outline-2",
-        "focus-visible:outline-offset-2",
-        "focus-visible:outline-indigo-600"
-      )
-
-      const icon = document.createElement('i')
-      icon.classList.add(
-        'fa-solid',
-        'fa-arrow-right'
-      )
-
-      link.appendChild(icon);
-
-      row.appendChild(label);
-      row.appendChild(link);
-
-      container.appendChild(row)
+      container.appendChild(session);
     })
   })
 
 
-fetch("/api/user/sessions/debug")
-  .then((response) => response.json())
-  .then((data) => {
-    const container = document.getElementById("sessions_debug")
-    container.innerText = JSON.stringify(data, null, 2)
-  })
+// fetch("/api/user/sessions/debug")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const container = document.getElementById("sessions_debug")
+//     container.innerText = JSON.stringify(data, null, 2)
+//   })
 
