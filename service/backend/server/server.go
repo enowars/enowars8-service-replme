f2d73ed88222fde02986f7dbb2ee97ba4039a53c
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-06-23 11:15:29 +0200

    fix: add waiting trigger for ci/cd

--- service/backend/server/server.go
+++ service/backend/server/server.go
@@ -1,8 +1,12 @@
 package server
 
-import "replme/service"
+import (
+	"replme/service"
+	"replme/util"
+)
 
 func Init(docker *service.DockerService) {
 	engine := NewRouter(docker)
+	util.SLogger.Infof("Server is running on port 6969")
 	engine.Run(":6969")
 }
